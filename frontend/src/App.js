d378d7f7ea558831736bad77d2d05176ee81b61e
sumitk-hub1999 <skatyal1999@gmail.com>
2023-11-02 06:34:26 +0530

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import { ProductList } from "./features/productslist/ProductList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
