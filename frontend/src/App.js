deda59cf0863a84c5ee8c6f9568b88ef4b1a179e
sumitk-hub1999 <skatyal1999@gmail.com>
2023-11-03 08:06:51 +0530

    day2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
-import { ProductList } from "./features/productslist/ProductList";
+// import { ProductList } from "./features/productslist/ProductList";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProductList />
+      <Home></Home>
     </div>
   );
 }
--- frontend/src/features/productslist/ProductList.js
+++ frontend/src/features/productslist/ProductList.js
@@ -238,7 +238,7 @@ export function ProductList() {
         <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-              New Arrivals
+              All Products
             </h1>
 
             <div className="flex items-center">
@@ -388,10 +388,6 @@ export function ProductList() {
               <div className="lg:col-span-3">
                 <div className="bg-white">
                   <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                      Customers also purchased
-                    </h2>
-
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                       {products.map((product) => (
                         <div key={product.id} className="group relative">
