b9bd04bea33d79f56b0ad573cb98403b6a06c3d1
sumitk-hub1999 <skatyal1999@gmail.com>
2023-11-13 20:21:00 +0530

    login signup

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,14 @@
 import Navbar from "../features/navbar/Navbar";
 import { ProductList } from "../features/productslist/ProductList";
+import LoginPage from "./Login";
+import SignUp from "../features/auth/components/SignUp";
 function Home() {
   return (
     <div>
       <Navbar>
-        <ProductList></ProductList>
+        {/* <LoginPage></LoginPage> */}
+        <SignUp />
+        {/* <ProductList></ProductList> */}
       </Navbar>
     </div>
   );
